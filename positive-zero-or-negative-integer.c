a7f87b8dcfdf3f966a121f80e97f89f85f17fe65
Pranish Pradhan <80395995+pranish-s-github@users.noreply.github.com>
2022-09-27 00:27:02 +0545

    Add files via upload

---
+++ positive-zero-or-negative-integer.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main() {
+    int number;
+    printf("Enter an integer: ");
+    scanf("%d", &number);
+
+    if (number > 0)
+    {
+        printf("%d is a positive integer.\n",number);
+    }
+    else if (number < 0) 
+    {
+        printf("%d is an negative integer.\n",number);
+    }
+
+    else 
+    {
+    	printf("%d is equal to zero, which is neither postive nor negative.\n",number);
+    }
+
+    return 0;
+}
