ca6f0a01dc1fdef3b184130aee4f08ef5a7b174c
Pranish Pradhan <80395995+pranish-s-github@users.noreply.github.com>
2022-09-30 00:03:06 +0545

    Update program-to-find-the-largest-number-using-if...else-statement.c

--- program-to-find-the-largest-number-using-if...else-statement.c
+++ program-to-find-the-largest-number-using-if...else-statement.c
@@ -10,16 +10,19 @@ int main() {
   printf("Enter third number: ");
   scanf("%lf", &num3);
 
-  if (num1 >= num2 && num1 >= num3)
+  if (num1 >= num2 && num1 >= num3) {
     printf("%lf is the largest number.", num1);
+  }
 
-  else if (num2 >= num1 && num2 >= num3)
+  else if (num2 >= num1 && num2 >= num3) {
     printf("%lf is the largest number.", num2);
-
+  }
+  
   // if both the above conditions are false then
   // num3 is the largest number
-  else
+  else {
     printf("%lf is the largest number.", num3);
-
+  }
+  
   return 0;
 }
