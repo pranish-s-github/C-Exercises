a25b1bce4447200cc225dab5c345e75c2b7f1f99
Pranish Pradhan <80395995+pranish-s-github@users.noreply.github.com>
2022-10-21 20:58:52 +0545

    Add files via upload

---
+++ add-two-numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+
+    int num1, num2;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &num1, &num2);
+
+    printf("The sum of the two numbers is %d.\n", num1 + num2);
+
+    return 0;
+
+}
